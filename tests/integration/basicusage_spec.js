4a69e9c597fbaf44620e0070e06a987d616b4732
Tormod Haugland <tormod.haugland@gmail.com>
2017-02-05 22:40:16 +0100

    Added some more basicusage tests

--- tests/integration/basicusage_spec.js
+++ tests/integration/basicusage_spec.js
@@ -1,29 +1,68 @@
-/* global describe, it, before, beforeEach, after, afterEach */
+/* global describe, it */
 import {expect} from 'chai';
 
-import PseudoFetch from '../../src/index.js';
+import pseudoFetch from '../../src/index.js';
 
 describe('Basic usage integration', () => {
   it('should handle a simple get request, and give us some data back', () => {
-    const server = PseudoFetch();
+    const server = pseudoFetch();
     server
       .get('/myroute')
       .send('Hello world');
 
     return expect(
       fetch('/myroute')
-        .then(response => response.body)
+        .then((response) => response.body)
     ).to.eventually.equal('Hello world');
   });
   it('should set a status code', () => {
-    const server = PseudoFetch();
+    const server = pseudoFetch();
     server
       .get('/myroute')
       .status(201);
 
     return expect(
       fetch('/myroute')
-        .then(response => response.status)
+        .then((response) => response.status)
     ).to.eventually.equal(201);
+  });
+  it('should handle only correct methods', () => {
+    const server = pseudoFetch();
+    server
+      .post('/myroute')
+      .send('test');
+
+    return Promise.all([
+      expect(
+        fetch('/myroute', {method: 'POST'})
+          .then((response) => response.status)
+      ).to.eventually.equal(200),
+      expect(
+        fetch('/myroute', {method: 'GET'})
+          .then((response) => response.status)
+      ).to.eventually.equal(404),
+    ]);
+  });
+  it('should be able to serve json objects', () => {
+    const server = pseudoFetch();
+    server
+      .get('/myroute')
+      .send({a: 2});
+
+    expect(
+      fetch('/myroute')
+        .then((response) => response.json())
+    ).to.eventually.deep.equal({a: 2});
+  });
+  it('should be able to serve json as strings', () => {
+    const server = pseudoFetch();
+    server
+      .get('/myroute')
+      .send('{"a": 2}');
+
+    expect(
+      fetch('/myroute')
+        .then((response) => response.json())
+    ).to.eventually.deep.equal({a: 2});
   });
 });
